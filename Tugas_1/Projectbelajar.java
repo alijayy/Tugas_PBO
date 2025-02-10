0e6515938a619f93f65140bf58f8c24564458645
Ali <153357185+alijayy@users.noreply.github.com>
2025-02-10 13:30:44 +0700

    Add files via upload
    Tugas 1 PBO

---
+++ Tugas_1/Projectbelajar.java
@@ -0,0 +1,10 @@
+package com.mycompany.projectbelajar;
+/**
+ *
+ * @author Ali Zainal Abidin Zaky - 2309033
+ */
+public class Projectbelajar {
+    public static void main(String[] args) {
+        System.out.println("\nHello World, Ali Zainal Abidin Zaky - 2309033 \nSaya Senang Belajar Java");
+    }
+}
