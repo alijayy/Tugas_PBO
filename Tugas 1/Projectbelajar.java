8f05b90d3fdd207fbd9c82bd8711794e3771f01a
Ali <153357185+alijayy@users.noreply.github.com>
2025-02-12 21:34:57 +0700

    Add files via upload

---
+++ Tugas 1/Projectbelajar.java
@@ -0,0 +1,11 @@
+package com.ali_zainal_abidin_zaky_2309033.projectbelajar;
+
+/**
+ *
+ * @author Ali Zaky
+ */
+public class Projectbelajar {
+    public static void main(String[] args) {
+        System.out.println("\nHello World, Ali Zainal Abidin Zaky - 2309033 \nSaya Senang Belajar Java");
+    }
+}
